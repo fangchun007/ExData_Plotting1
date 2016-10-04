fbd772bc94b77722ab9af8e2e5fe700a5153060f
fangchun007 <fangchun007@gmail.com>
2016-10-04 22:31:27 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -38,14 +38,14 @@ close(con) # close the connection
 
 par(mfrow = c(2, 2), mar = c(4, 4, 2, 2 ))
 with(householdData, {
-	plot(DateTime, Global_active_power, type = "l", 
- xlab = " ", ylab = "Global Active Power")
+    plot(DateTime, Global_active_power, type = "l", 
+      xlab = " ", ylab = "Global Active Power")
     plot(DateTime, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
     plot(DateTime, Sub_metering_1, type = "l", xlab = " ", ylab = "Energy sub metering")
     lines(DateTime, Sub_metering_2, col = "red")
     lines(DateTime, Sub_metering_3, col = "blue")
     legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend =
-     c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+      c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime")
  })
 
